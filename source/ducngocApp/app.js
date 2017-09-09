cec884e12b8005815c9fd00c6ed85f0b0fdc74ff
TruongTienDuc <ductt.it.hust@gmail.com>
2017-09-09 14:41:44 +0700

    upatate

--- source/ducngocApp/app.js
+++ source/ducngocApp/app.js
@@ -9,6 +9,23 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//#endregion
+
+//#region Connect Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error '+err);
+});
+
 //#endregion
 
 //#region Initialize server
@@ -16,19 +33,21 @@ const app = express();
 const port = 6789;
 
 const users = require('./routes/users');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use('/users',users);
+app.use('/users', users);
 
-app.listen(port,()=>{
-    console.log('Server started on port '+port);
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
 });
 //#endregion 
 
 //#region Api
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
 //#endregion
