bc06b62743990d7a5f3ba53ea92d5bc47116cea7
ADMINISTRATOR\Administrator <bichngocbk1994@gmail.com>
2017-09-19 21:07:59 +0700

    update

--- source/Angular4/angular-tour-of-heroes/src/app/app.component.ts
+++ source/Angular4/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,37 +1,27 @@
+import { HeroService } from './hero.service';
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
+  constructor(private heroService: HeroService) {
+    
+   }
   title = 'Tour of heroes';
-  heroes =['Windstorm','Bombasto','Magneta','Tornado'];
-  myHeroes= this.heroes[0];
-  hero: Hero={
-    id:1,
-    name: 'windStorm'
-  };
-public heroe = HEROES;
-selectedHero: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-}
-export class Hero{
-  id: number;
-  name: string;
-}
+  heroes: Hero[];
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
 
-
-const HEROES: Hero[]=[{ id: 11, name: 'Mr. Nice' },
-{ id: 12, name: 'Narco' },
-{ id: 13, name: 'Bombasto' },
-{ id: 14, name: 'Celeritas' },
-{ id: 15, name: 'Magneta' },
-{ id: 16, name: 'RubberMan' },
-{ id: 17, name: 'Dynama' },
-{ id: 18, name: 'Dr IQ' },
-{ id: 19, name: 'Magma' },
-{ id: 20, name: 'Tornado' }];
+  }
+ 
+}
+// export class Hero{
+//   id: number;
+//   name: string;
+// }
